5fa4220a598f04fbf4c226079d7b74af4deff0c7
Yerofey S <pm@yerofey.dev>
2023-01-09 18:28:00 +0100

    feat(main): remove storage cleaning

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@ import App from './App.vue';
 import router from './router';
 
 // delete old items
-import Storage from './modules/storage';
-const storage = new Storage();
-const oldPrefix = 'secred__';
-if (Object.values(storage.getAllItems(oldPrefix)).length > 0) {
-  storage.removeAllItems(oldPrefix);
-}
+// import Storage from './modules/storage';
+// const storage = new Storage();
+// const oldPrefix = 'secred__';
+// if (Object.values(storage.getAllItems(oldPrefix)).length > 0) {
+//   storage.removeAllItems(oldPrefix);
+// }
 
 import 'modern-normalize/modern-normalize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
