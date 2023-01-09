cb4f63cb4e36d06ca03f9734a23a35c6318edcc3
Yerofey S <pm@yerofey.dev>
2023-01-09 17:43:20 +0100

    fix(main): delete items with old prefix and structure

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import { createApp } from 'vue';
 // import { BootstrapIconsPlugin } from 'bootstrap-icons-vue';
 import VueCryptojs from 'vue-cryptojs';
-// import Vue3Storage from 'vue3-storage';
 import App from './App.vue';
-// import './registerServiceWorker';
 import router from './router';
+
+// delete old items
+import Storage from './modules/storage';
+const storage = new Storage();
+const oldPrefix = 'secred__';
+if (Object.values(storage.getAllItems(oldPrefix)).length > 0) {
+  storage.removeAllItems(oldPrefix);
+}
 
 import 'modern-normalize/modern-normalize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
