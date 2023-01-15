65a37f36dfc2ba9d7bdfc306e9734fd9e513876e
Yerofey S <pm@yerofey.dev>
2023-01-16 00:15:09 +0100

    feat(home): add autofocus for textarea

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,6 +12,7 @@
         :placeholder="`${$t('home.form.insert')}...`"
         autocorrect="off"
         v-model="secretContent"
+        v-focus
       ></textarea>
     </div>
     <div class="group-optional mt-4 mb-4">
