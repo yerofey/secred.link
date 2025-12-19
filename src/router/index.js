943b1beb0e7a4d0e5d65a7de5f50afa2e7c3be1e
Yerofey <erofeysmirnov@gmail.com>
2025-12-19 11:24:25 +0100

    refactor: clean up router configuration by removing commented-out code

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Storage from '../views/Storage.vue';
 import ManageSecret from '../views/ManageSecret.vue';
-// import DeleteSecret from '../views/_DeleteSecret.vue';
 import ViewSecret from '../views/ViewSecret.vue';
-
-// base: import.meta.env.BASE_URL,
 
 const routes = [
   {
@@ -34,14 +31,6 @@ const routes = [
     name: 'view',
     component: ViewSecret,
   },
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-  // },
 ];
 
 const router = createRouter({
